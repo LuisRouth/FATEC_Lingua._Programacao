f9797ca1239b57680d3254f510a401b2dd8a3060
Luis Otavio Routh da Silva <luisotaviorouth@gmail.com>
2025-04-18 11:58:32 -0300

    Aula5
    Quinta aula

---
+++ AulaP-5/Fixação/teste.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+ return (a + b)
+
+x = soma(10,20)
+print (x)
+
+def teste(refrigerante, suco, pastel, lanche):
+ return (refrigerante + pastel) - (suco + lanche)
+
+y = teste(110,90,40,60)
+print (y)
