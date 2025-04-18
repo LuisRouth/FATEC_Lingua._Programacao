f931a7ec995c4cb326993d91be086aed6faf4128
Luis Otavio Routh da Silva <luisotaviorouth@gmail.com>
2025-04-18 11:29:37 -0300

    Aula1
    Primeira aula

---
+++ AulaP-1/Primeiros passos/a.py
@@ -0,0 +1,20 @@
+'''
+variavel1 = 10 uma "caixinha" 
+variavel1 =variavel1 * 2 
+nome = "Luis Otavio" 
+type(variavel1) descobrir qual a variavel
+numericos , ponto flutuante(float), inteiros(int), Sting(dados pessoais)
+variavel2 = 5.4 type(variavel2)
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ") idade é string. para converter é
+idade = int(idade) ou ja direto: idade = int(input("Digite sua idade: "))
+sobrenome = input("Digite seu sobrenome: )
+print = nome + " " + sobrenome
+'''
+#aula2
+variavel1 = 10
+#Inteiro (int)
+variavel2= 8.5
+#ponto flutuante(float)
+variavel3 = "Fatec Rio Claro"
+#valor textual String
